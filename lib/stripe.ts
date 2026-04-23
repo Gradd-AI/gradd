5fd490b3c90d1799a221562f3af2e172dbaffac3
Gradd-AI <grant@live.ie>
2026-04-23 09:06:42 +0100

    fix stripe api version

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,8 +1,7 @@
 import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2024-06-20',
-  typescript: true,
+  apiVersion: '2026-03-25.dahlia',
 });
 
 export default stripe;
